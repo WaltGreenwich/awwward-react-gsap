97e92360bb7728c8d1ad1c391ccad6a75c1624ed
WGreenwichChaca <walter.greenwich@gmail.com>
2025-04-22 12:55:30 -0500

    Agregando bash.exe.stackdump

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <main className="bg-red-300 font-bold text-5xl">
+    <main className="bg-red-300 font-bold text-5xl font-circularWeb">
       <h1>Welcome to Awwwards</h1>
     </main>
   );
