8e7f062cfefea54bdfd275b55f8a8b84b82d0d45
WGreenwichChaca <walter.greenwich@gmail.com>
2025-05-25 22:48:59 -0500

    configurando tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ export default {
         robertMedium: ["robert-medium", "sans-serif"],
         robertRegular: ["robert-regular", "sans-serif"],
       },
+      screens: {
+        xs: "375px", // si tú creaste este breakpoint personalizado
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [],
